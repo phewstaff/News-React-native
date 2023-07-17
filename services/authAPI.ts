7213eb1fc618a40c0a7345abda091e683b1b1c4e
magomed_n <naurbaev2020@gmail.com>
2023-07-17 12:11:13 +0300

    stored response data in redux

--- services/authAPI.ts
+++ services/authAPI.ts
@@ -1,21 +1,29 @@
 import { NavigationProp } from '@react-navigation/native';
+import { ApiResponse } from 'apisauce';
+
 import { api } from '.';
-import { FormData } from '../components/AuthForm';
 import { RootStackParams } from '../App';
+import { FormData } from '../components/AuthForm';
+import { AppDispatch } from '../redux/store';
+import { setAuthenticated, setUser } from '../redux/userSlice';
+import { IUser } from '../types';
 
 export const signIn = async (
   data: FormData,
   navigation: NavigationProp<RootStackParams>,
-) => {
+  dispatch: AppDispatch,
+): Promise<IUser | unknown> => {
   try {
-    const response = await api.post('/auth/sign_in', data);
+    const response: ApiResponse<IUser> = await api.post('/auth/sign_in', data);
 
-    if (response.ok) {
+    if (response.ok && response.data) {
+      dispatch(setAuthenticated(true));
+      dispatch(setUser(response.data));
       navigation.navigate('News');
 
       if (response.headers) {
         api.setHeaders({
-          accessToken: response.headers['access-token'],
+          'access-token': response.headers['access-token'],
           uid: response.headers.uid,
           client: response.headers.client,
         });
@@ -23,9 +31,10 @@ export const signIn = async (
 
       return response.data;
     } else {
-      return null;
+      return 'Неправильный логин или пароль';
     }
   } catch (error) {
+    ('Произошла ошибка, попробуйте позже');
     return error;
   }
 };
