9dae40badc7f1175e92210073dcfce4211945b77
magomed_n <naurbaev2020@gmail.com>
2023-07-18 07:26:29 +0300

    reorganized fetching data to store

--- redux/newsSlice.ts
+++ redux/newsSlice.ts
@@ -1,69 +1,69 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-import { getNews, getNewsById } from '../services/newsAPI';
 import { INewsItem } from '../types';
 
 interface NewsState {
   loading: boolean;
   error: string | null;
-  data: INewsItem[];
+  data: INewsItem[] | undefined;
   selectedNews: INewsItem | null;
+  selectedNewsLoading: boolean;
+  selectedNewsError: string | null;
 }
 
 const initialState: NewsState = {
   loading: false,
   error: null,
-  data: [],
+  data: undefined,
   selectedNews: null,
+  selectedNewsLoading: false,
+  selectedNewsError: null,
 };
-
-export const fetchNewsData = createAsyncThunk(
-  'news/fetchNewsData',
-  async () => {
-    const response = await getNews();
-    return response as INewsItem[];
-  },
-);
-
-export const fetchNewsById = createAsyncThunk(
-  'news/fetchNewsById',
-  async (newsId: string) => {
-    const response = await getNewsById(newsId);
-    return response as INewsItem;
-  },
-);
 
 export const newsSlice = createSlice({
   name: 'news',
   initialState,
-  reducers: {},
-  extraReducers: builder => {
-    builder
-      .addCase(fetchNewsData.pending, state => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchNewsData.fulfilled, (state, action) => {
-        state.loading = false;
-        state.data = action.payload;
-      })
-      .addCase(fetchNewsData.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || 'Failed to fetch news';
-      })
-      .addCase(fetchNewsById.pending, state => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchNewsById.fulfilled, (state, action) => {
-        state.loading = false;
-        state.selectedNews = action.payload;
-      })
-      .addCase(fetchNewsById.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || 'Failed to fetch news article';
-      });
+  reducers: {
+    fetchNewsStart: state => {
+      state.loading = true;
+      state.error = null;
+    },
+
+    fetchNewsfulfilled: (state, action: PayloadAction<INewsItem[]>) => {
+      state.loading = false;
+      state.error = null;
+      state.data = action.payload;
+    },
+
+    fetchNewsFailure: state => {
+      state.loading = false;
+      state.error = 'Произошла ошибка при загрузке новостей';
+    },
+    fetchSelectedNewsStart: state => {
+      state.selectedNewsLoading = true;
+      state.selectedNewsError = null;
+    },
+
+    fetchSelectedNewsfulfilled: (state, action: PayloadAction<INewsItem>) => {
+      state.selectedNewsLoading = false;
+      state.selectedNewsError = null;
+      state.selectedNews = action.payload;
+    },
+
+    fetchSelectedNewsFailure: state => {
+      state.selectedNewsLoading = false;
+      state.selectedNewsError = 'Произошла ошибка при загрузке новости';
+    },
   },
 });
+
+export const {
+  fetchNewsStart,
+  fetchNewsfulfilled,
+  fetchNewsFailure,
+  fetchSelectedNewsStart,
+  fetchSelectedNewsfulfilled,
+  fetchSelectedNewsFailure,
+} = newsSlice.actions;
 
 export default newsSlice.reducer;
